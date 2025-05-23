789bfe20d5fee271effc4fc9d1a4c0a4cc41750c
Rex Perez <matthewrperez@yahoo.com>
2025-05-22 22:34:03 -0500

    added a print statement and changedhow much was being added

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def add(a, b):
     return a + b
 
 def main():
-    result = add(5, 5)
+    result = add(5, 90)
     print(f"The sum is: {result}")
+    print("This is the edit that i added on my mac")
 if __name__ == "__main__":
     main()
